7d3dedbe621c75fe84e2374421b527e03ed3d923
Wander Hillen <wjw.hillen@gmail.com>
2024-05-22 18:06:07 +0200

    Rudimentary run function and basic error reporting

--- app/Main.hs
+++ app/Main.hs
@@ -7,13 +7,14 @@ import qualified Data.Text.IO as TIO
 import System.Environment
 import System.Exit
 
-data InterpreterContext = NoContext deriving (Show,Eq) -- for now, to make it compile
+data InterpreterContext = Context { hadError :: Bool } deriving (Show,Eq) -- for now, to make it compile
+emptyContext = Context False
 
 main :: IO ()
 main = do
   args <- getArgs
   _ <- case args of
-    [] -> runPrompt NoContext
+    [] -> runPrompt emptyContext
     (filename:[]) -> runFile filename
     _ -> do
       putStrLn "Usage: hlox [script]"
@@ -23,7 +24,10 @@ main = do
 runFile :: FilePath -> IO InterpreterContext
 runFile filepath = do
   contents <- TIO.readFile filepath
-  run NoContext contents
+  finalContext <- run emptyContext contents
+  case hadError finalContext of
+    True -> exitWith (ExitFailure 65)
+    False -> return finalContext
 
 runPrompt :: InterpreterContext -> IO InterpreterContext
 runPrompt context = do
@@ -33,8 +37,24 @@ runPrompt context = do
     "" -> return context
     _ -> do
       newContext <- run context nextLine
+      resetErrors
       runPrompt newContext
 
+resetErrors = undefined
 
 run :: InterpreterContext -> T.Text -> IO InterpreterContext
-run = undefined
+run context input = do
+  tokens <- scanner input
+  mapM_ print tokens
+  return context
+
+
+error :: Int -> String -> IO ()
+error linenr message = report linenr "" message
+
+-- according to the book this should also set a global "hadError" boolean to true
+report :: Int -> String -> String -> IO ()
+report linenr location message = do
+  putStrLn $ concat ["[line ", show linenr, "] Error", location, ": ", message]
+
+scanner = undefined
