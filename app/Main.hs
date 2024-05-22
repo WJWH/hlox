9c1606d7bf2ea6919fb0109015f4dac98771371b
Wander Hillen <wjw.hillen@gmail.com>
2024-05-22 17:07:47 +0200

    Set up file and repl modes

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,40 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
+import qualified Data.Text as T
+import qualified Data.Text.IO as TIO
+import System.Environment
+import System.Exit
+
+data InterpreterContext = NoContext deriving (Show,Eq) -- for now, to make it compile
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+  args <- getArgs
+  _ <- case args of
+    [] -> runPrompt NoContext
+    (filename:[]) -> runFile filename
+    _ -> do
+      putStrLn "Usage: hlox [script]"
+      exitWith (ExitFailure 64)
+  return ()
+
+runFile :: FilePath -> IO InterpreterContext
+runFile filepath = do
+  contents <- TIO.readFile filepath
+  run NoContext contents
+
+runPrompt :: InterpreterContext -> IO InterpreterContext
+runPrompt context = do
+  putStr "> "
+  nextLine <- TIO.getLine
+  case nextLine of
+    "" -> return context
+    _ -> do
+      newContext <- run context nextLine
+      runPrompt newContext
+
+
+run :: InterpreterContext -> T.Text -> IO InterpreterContext
+run = undefined
