9678248ebceb6485d5da92ba50bb45f05886bb00
Wander Hillen <wjw.hillen@gmail.com>
2024-06-17 17:41:10 +0200

    Bang out interpreter for these expressions

--- app/Interpreter.hs
+++ app/Interpreter.hs
@@ -1,23 +1,94 @@
 module Interpreter where
 
+import Control.Monad.Except
+import Control.Monad.State
+
 import Types
 
--- Inxpired by https://github.com/ccntrq/loxomotive/blob/master/src/Loxomotive/Interpreter.hs,
+-- Inspired by https://github.com/ccntrq/loxomotive/blob/master/src/Loxomotive/Interpreter.hs,
 -- the core interpreter type is an ExceptT StateT IO value:
--- - ExceptT so that we can throw exceptions from anywhere without having to wrap/unwrap Eithers,
+-- - ExceptT so that we can throw exceptions from anywhere without having to wrap/unwrap Eithers all the time
 -- - StateT to keep state about variable bindings etc
 -- - IO as base monad is required because the PRINT method is baked right into the language
-type Interpreter = undefined
+type Interpreter = ExceptT InterpreterError (StateT InterpreterState IO)
+data InterpreterState = InterpreterState {  } deriving (Show,Eq)
+data InterpreterError = ArgumentError { description :: String }
+                      deriving (Show,Eq)
 
-evaluate :: Expression -> RuntimeValue
+newInterpreterState = InterpreterState
+
+-- and then:
+runInterpreter :: InterpreterState -> Interpreter a -> IO (Either InterpreterError a, InterpreterState)
+runInterpreter st i = runStateT (runExceptT i) st
+
+interpret :: Expression -> IO ()
+interpret expr = do
+  (result, finalState) <- runInterpreter newInterpreterState (evaluate expr)
+  either (\err -> print err) (\res -> print res) result
+
+evaluate :: Expression -> Interpreter RuntimeValue
+evaluate (Literal (NumberLit num)) = return $ Number num
+evaluate (Literal (StringLit str)) = return $ String str
+evaluate (Literal TrueLit) = return $ Boolean True
+evaluate (Literal FalseLit) = return $ Boolean False
+evaluate (Literal NilLit) = return $ Null
 evaluate (Grouping expr) = evaluate expr
+-- Can only be numerical negation (for numbers only obv) or logical negation
 evaluate (Unary op expr) = case op of
-  Negate -> undefined
-  Bang -> undefined
-evaluate (Binary op left right) = undefined
-evaluate (Literal (NumberLit num)) = Number num
-evaluate (Literal (StringLit str)) = String str
-evaluate (Literal TrueLit) = Boolean True
-evaluate (Literal FalseLit) = Boolean False
-evaluate (Literal NilLit) = Null
+  Negate -> do
+    exprVal <- evaluate expr
+    case exprVal of
+      Number num -> return . Number $ negate num
+      _ -> throwError $ ArgumentError "Can only negate numbers"
+  Bang -> do
+    exprVal <- evaluate expr
+    return $ if isTruthy exprVal then Boolean False else Boolean True
+-- Binary operators usually need their operators to be either two numbers (for +-*/) or
+-- two Booleans (for <>== etc), except for `+` which can take two numbers but also two strings.
+-- In all cases we evaluate both sides first
+evaluate (Binary op left right) = do
+  leftVal <- evaluate left
+  rightVal <- evaluate right
+  case op of
+    DoubleEqual -> return . Boolean $ leftVal == rightVal
+    NotEqual -> return . Boolean $ leftVal /= rightVal
+    LessThan -> do
+      ensureBothNumber LessThan leftVal rightVal
+      return . Boolean $ (numVal leftVal) < (numVal rightVal)
+    LessEqualThan -> do
+      ensureBothNumber LessEqualThan leftVal rightVal
+      return . Boolean $ (numVal leftVal) <= (numVal rightVal)
+    GreaterThan -> do
+      ensureBothNumber GreaterThan leftVal rightVal
+      return . Boolean $ (numVal leftVal) > (numVal rightVal)
+    GreaterEqualThan -> do
+      ensureBothNumber GreaterEqualThan leftVal rightVal
+      return . Boolean $ (numVal leftVal) >= (numVal rightVal)
+    Subtract -> do
+      ensureBothNumber Subtract leftVal rightVal
+      return . Number $ (numVal leftVal) - (numVal rightVal)
+    Multiply -> do
+      ensureBothNumber Multiply leftVal rightVal
+      return . Number $ (numVal leftVal) * (numVal rightVal)
+    Divide -> do
+      ensureBothNumber Divide leftVal rightVal
+      return . Number $ (numVal leftVal) / (numVal rightVal)
+    Add -> case (leftVal, rightVal) of
+      (Number l, Number r) -> return . Number $ l + r
+      (String ll, String rr) -> return . String $ ll ++ rr
+      _ -> throwError $ ArgumentError ("Arguments to Plus must be either two Numbers or two Strings")
 
+-- Utility functions
+isTruthy :: RuntimeValue -> Bool
+isTruthy Null = False
+isTruthy (Boolean False) = False
+isTruthy _ = True
+
+numVal :: RuntimeValue -> Double
+numVal (Number num) = num
+numVal _ = error "Unreachable, tried to call numVal on non-number runtime value"
+
+-- Will throw an exception unless both values are Numbers
+ensureBothNumber :: BinaryOperation -> RuntimeValue -> RuntimeValue -> Interpreter ()
+ensureBothNumber op (Number _) (Number _) = return ()
+ensureBothNumber op _ _ = throwError $ ArgumentError ("Both arguments to " ++ show op ++ " must be Numbers")
